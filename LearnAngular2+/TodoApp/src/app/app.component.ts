6f8f9bf906d8b03458a96fb8349ee3d35761fa0f
Anderson Conceição <andersonconceiicao@gmail.com>
2020-08-13 15:27:58 -0300

    Conclusao do ToDo. Sem estilização

--- LearnAngular2+/TodoApp/src/app/app.component.ts
+++ LearnAngular2+/TodoApp/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { Todo } from './Models/TodoModel';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,16 +18,14 @@ export class AppComponent {
     this.form = this.fb.group({
       titulo: ['', [Validators.maxLength(60), Validators.minLength(3), Validators.required]]
     });
-
-    this.todos.push(new Todo(1, 'feito', false));
-    this.todos.push(new Todo(2, 'Le Livros', false));
-    this.todos.push(new Todo(3, 'Le Livros', false));
+    this.Load();
   }
   Adicionar(): void
   {
     const titulo = this.form.controls[`titulo`].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, titulo, false));
+    this.Save();
     this.Clear();
   }
   Apagar(todo: Todo): void {
@@ -34,10 +33,20 @@ export class AppComponent {
     if (index !== -1 ){
       this.todos.splice(index, 1);
     }
+    this.Save();
   }
   Alterar(todo: Todo): void {
     todo.Ativo = !todo.Ativo;
+    this.Save();
   }
+  Save(): void{
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+  Load(): void{
+    const data = localStorage.getItem('todos');
+    data != null ? this.todos = JSON.parse(data) : this.todos = [];
+    }
   Clear(): void{
     this.form.reset();
   }
