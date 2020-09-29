d4ebd14041f17a4f5b9748e75e5aecb87dbb8a37
Anderson Conceição <andersonconceiicao@gmail.com>
2020-09-29 17:07:09 -0300

    add model e services

---
+++ eshop/src/app/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataService {
+    baseUrl = 'http://localhost:7188';
+
+constructor(private http: HttpClient) {}
+
+    authenticate(data: any)
+    {
+        return this.http.post(`${this.baseUrl}/v1/login`, data);
+    }
+
+    getMonthlySalesChartData()
+    {
+        return this.http.get(`${this.baseUrl}/v1/reports/ms`);
+    }
+    getOrders()
+    {
+        return this.http.get(`${this.baseUrl}/v1/orders`);
+    }
+    getOrder(order: string)
+    {
+        return this.http.get(`${this.baseUrl}/v1/orders/${order}`);
+    }
+
+}
---
+++ eshop/src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+export class UserModel {
+    constructor(
+        email: string,
+        image: string,
+        name: string,
+        roles: string[],
+        tokeen: string,
+        username: string
+    ) {}
+
+// tslint:disable-next-line: eofline
+}
